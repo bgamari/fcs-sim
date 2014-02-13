b346461579b687b4fcc78d28569529f910d6f180
Ben Gamari <bgamari.foss@gmail.com>
2014-02-13 12:23:08 -0500

    OOops

--- RunSim.hs
+++ RunSim.hs
@@ -36,5 +36,5 @@ boxSize = 15 *^ beamWidth
 
 evolve :: RVarT IO ()
 evolve = do
-    traj <- evolveParticle boxSize beamWidth
+    traj <- evolveParticle boxSize sigma
     liftIO $ V.mapM_ print traj
