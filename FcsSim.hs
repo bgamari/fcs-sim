2a637b81171808f8d82a2a7e8beeef1a9d151240
Ben Gamari <ben@smart-cactus.org>
2015-04-18 09:50:53 -0400

    FcsSim: Add Spherical type

--- FcsSim.hs
+++ FcsSim.hs
@@ -13,6 +13,28 @@ import Control.Monad
 import Control.Monad.Primitive (PrimMonad(..))
 import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Storable as VS
+import Control.Lens
+
+data Spherical a = Spherical { _r      :: !a    -- ^ radial
+                             , _theta  :: !a    -- ^ inclination
+                             , _phi    :: !a    -- ^ azimuth
+                             }
+                 deriving (Show, Eq, Ord)
+
+sphericalV3 :: RealFloat a => Iso' (Spherical a) (V3 a)
+sphericalV3 = iso from to
+  where
+    from (Spherical r theta phi) = V3 x y z
+      where
+        x = r * sin theta * cos phi
+        y = r * sin theta * sin phi
+        z = r * cos theta
+    to (V3 x y z) = Spherical r theta phi
+      where
+        r = sqrt (x^2 + y^2 + z^2)
+        theta = acos (z / r)
+        phi = atan2 y x
+{-# INLINE sphericalV3 #-}
 
 type Diffusivity = Double
 type Time = Double
