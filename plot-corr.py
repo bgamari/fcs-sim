plot-corr: Fix standard error computation

plot-corr.py
@@ -59,7 +59,7 @@ pl.suptitle('Molecule diffusion inside "sticky" droplet, simulated')
 #pl.hist2d(np.log10(corrs['tau'].flatten()), corrs['g'].flatten(), bins=(100,100), vmax=300)
 
 mu = np.mean(corrs['g'], axis=0)
-err = np.std(corrs['g'], axis=0)
+err = np.std(corrs['g'], axis=0) / np.sqrt(corrs.shape[0])
 print(np.nonzero(np.logical_not(np.isfinite(corrs['g']))))
 pl.errorbar(corrs['tau'][0], mu, yerr=err, c='k', ecolor='0.5', linewidth=1)
 pl.axhline(0, c='k')